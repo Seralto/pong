291ecd35676821f710a29264d50c7753c1cdb2dd
Sérgio Toledo <contato@sergiotoledo.com.br>
2018-06-06 02:01:11 -0300

    move ball

--- game.js
+++ game.js
@@ -1,10 +1,39 @@
 let canvas = document.getElementById('canvas')
 let context = canvas.getContext('2d')
 
-drawRect = (coordX, coordY, width, heigth, color) => {
+drawRect = function(coordX, coordY, width, heigth, color) {
   context.fillStyle = color
   context.fillRect(coordX, coordY, width, heigth)
 }
 
-drawRect(0, 0, canvas.width, canvas.height, 'black')
-drawRect(400, 300, 10, 10, 'white')
+drawScreen = function() {
+  drawRect(0, 0, canvas.width, canvas.height, 'black')
+}
+
+let ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  speed: 1,
+  radius: 10,
+  color: 'white',
+  draw() {
+    drawRect(this.x, this.y, this.radius, this.radius, this.color)
+  },
+  move() {
+    this.x += this.speed
+  },
+  reboundX() {
+    if(this.x > canvas.width - this.radius || this.x < 0) {
+      this.speed = -this.speed
+    }
+  }
+}
+
+window.onload = function() {
+  setInterval(function() {
+    drawScreen()
+    ball.move()
+    ball.draw()
+    ball.reboundX()
+  })
+}

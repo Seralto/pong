3d5aaf6b3dcd30eb52146e843a9e3b6c12f30bb7
Sérgio Toledo <contato@sergiotoledo.com.br>
2018-06-07 00:33:36 -0300

    add player paddle and adjustments

--- game.js
+++ game.js
@@ -1,39 +1,64 @@
-let canvas = document.getElementById('canvas')
-let context = canvas.getContext('2d')
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
 
-drawRect = function(coordX, coordY, width, heigth, color) {
+const PADDLE_HEIGHT = 100
+
+drawRect = (coordX, coordY, width, heigth, color) => {
   context.fillStyle = color
   context.fillRect(coordX, coordY, width, heigth)
 }
 
-drawScreen = function() {
+drawScreen = () => {
   drawRect(0, 0, canvas.width, canvas.height, 'black')
 }
 
-let ball = {
+const ball = {
   x: canvas.width / 2,
   y: canvas.height / 2,
-  speed: 1,
-  radius: 10,
+  speedX: Math.random() + 1,
+  speedY: Math.random() * Math.random() < 0.5 ? - 1 : 1,
+  radius: 6,
   color: 'white',
   draw() {
-    drawRect(this.x, this.y, this.radius, this.radius, this.color)
+    context.beginPath()
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+    context.fillStyle = this.color;
+    context.fill();
   },
   move() {
-    this.x += this.speed
+    this.x += this.speedX
+    this.y += this.speedY
   },
-  reboundX() {
+  rebound() {
     if(this.x > canvas.width - this.radius || this.x < 0) {
-      this.speed = -this.speed
+      this.speedX = -this.speedX
+    }
+    if(this.y > canvas.height - this.radius || this.y < 0) {
+      this.speedY = -this.speedY
     }
   }
 }
 
-window.onload = function() {
-  setInterval(function() {
+const paddlePlayer = {
+  x: 10,
+  y: canvas.height / 2 - PADDLE_HEIGHT / 2,
+  speed: 10,
+  width: 10,
+  height: PADDLE_HEIGHT,
+  color: 'white',
+  draw(y) {
+    drawRect(this.x, this.y, this.width, this.height, this.color)
+  }
+}
+
+window.addEventListener("mousemove", evt => paddlePlayer.y = evt.clientY - 50)
+
+window.onload = () => {
+  setInterval(() => {
     drawScreen()
-    ball.move()
     ball.draw()
-    ball.reboundX()
+    ball.move()
+    ball.rebound()
+    paddlePlayer.draw()
   })
 }

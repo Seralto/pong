50fb0372ac5ff1c5b0a5dc2437a9233c39510f0f
Sérgio Toledo <contato@sergiotoledo.com.br>
2018-06-15 23:50:29 -0300

    add score

--- game.js
+++ game.js
@@ -5,6 +5,27 @@ const PADDLE_HEIGHT = 100
 const PADDLE_WIDTH = 10
 const PADDLE_DISTANCE = 5
 
+const score = {
+  player: 0,
+  computer: 0,
+  point() {
+    if(ball.x < 0) {
+      this.computer += 1
+      ball.reset()
+    }
+
+    if(ball.x > canvas.width) {
+      this.player += 1
+      ball.reset()
+    }
+
+    context.font = "30px Arial"
+    context.textAlign = "center"
+    context.fillText(this.player, 80, 80)
+    context.fillText(this.computer, canvas.width - 80, 80)
+  }
+}
+
 drawRect = (coordX, coordY, width, heigth, color) => {
   context.fillStyle = color
   context.fillRect(coordX, coordY, width, heigth)
@@ -48,6 +69,12 @@ const ball = {
     this.y < computerPaddle.y + PADDLE_HEIGHT ) {
       this.speedX = -this.speedX
     }
+  },
+  reset() {
+    this.x = canvas.width / 2
+    this.y = canvas.height / 2
+    this.speedX = Math.random() + 0.5,
+    this.speedY = Math.random() * Math.random() * .3 < 0.5 ? - 1 : 1
   }
 }
 
@@ -98,5 +125,6 @@ window.onload = () => {
     playerPaddle.draw()
     computerPaddle.draw()
     computerPaddle.move()
+    score.point()
   })
 }
